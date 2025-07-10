e853f1996b89b3ffc20a26560837c71fa8741414
Mharia <ms5132425@gmail.com>
2025-07-10 00:44:56 -0300

    feedback funcionando

--- index.php
+++ index.php
@@ -1,45 +1,107 @@
 <?php include "includes/cab.php"; ?>
 
-    <div class="info">
-        <div>
-            <img src="" alt="foto da profissional" class="foto1">
-            <p class="texto-foto1">ESPECIALISTA EM ALONGAMENTO DE <br> CILIOS E SOBRANCELHA</p>
+<div class="info">
+    <div>
+        <img src="" alt="foto da profissional" class="foto1">
+        <p class="texto-foto1">ESPECIALISTA EM ALONGAMENTO DE <br>CILIOS E DESIGNER SOBRANCELHA</p>
+    </div>
+    <div class="titulo">
+        <h1>LASH & EYEBROW <br>DESIGNER</h1>
+        <h2 class="sub">transforme seu olhar, realce sua beleza </h2>
+    </div>
 
-        </div>
-        <div class="linha"></div>
-        <div class="titulo">
-            <h1 class="principal">LASH & EYEBROW <br>DESIGNER</h1>
-            <h2 class="sub">transforme seu olhar, realce sua beleza </h2>
+</div>
+
+
+<div class="conf">
+    <div class="autoplay">
+        <img src="imagens/Rectangle 11.png">
+        <img src="imagens/Rectangle 10.png">
+        <img src="imagens/Rectangle 9.png">
+        <img src="imagens/Rectangle 7.png">
+
+    </div>
+</div>
+
+<?php include "includes/conexao.php" ?>
+<div class="fundo">
+
+    <?php
+    $sql = "select * from feedback";
+    $resultado = mysqli_query($conexao, $sql);
+    ?>
+    <div class="">
+
+
+        <h1 class="titulo-geral">FEDBACK'S</h1>
+
+        <div id="carouselExample" class="carousel slide">
+            <div class="carousel-inner ">
+                <?php
+                $ativo = true;
+                while ($linha = mysqli_fetch_assoc($resultado)) {
+                    echo '<div class="carousel-item' . ($ativo ? ' active' : '') . '">';
+                    echo '<div class="texto-feed">';
+                    echo '<p>" ' . $linha['retorno'] . ' "</p>';
+                    echo '<h1>- ' . $linha['nome'] . '</h1>';
+                    echo '</div></div>';
+                    $ativo = false;
+
+
+                }
+                ?>
+            </div>
+            
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+
         </div>
 
     </div>
 
 
-    
-    <div class="conf">
-        <div class="autoplay">
-            <img src="imagens/Rectangle 11.png">
-            <img src="imagens/Rectangle 10.png">
-            <img src="imagens/Rectangle 9.png">
-            <img src="imagens/Rectangle 7.png">
+</div>
+<?php
 
-        </div>
+mysqli_close($conexao);
+?>
+</div>
+
+
+
+</div>
+
+<div class="conf">
+    <div class="autoplay">
+        <img src="imagens/Rectangle 11.png">
+        <img src="imagens/Rectangle 10.png">
+        <img src="imagens/Rectangle 9.png">
+        <img src="imagens/Rectangle 7.png">
+
     </div>
+</div>
 
 
-    <div class="fundo">
-        <h1 class="titulo-fed">FEDBACK'S</h1>
-        <img src="">
 
-       
+<script src="funcoes.js/jquery.js"></script>
+<script src="lib/slick-1.8.1/slick-1.8.1/slick/slick.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q"
+    crossorigin="anonymous"></script>
+<script src="funcoes.js/app.js"></script>
 
-
+<footer class="rodape">
+    <div>
+        <p class="texto-rodape"> BIANCA SERAFIM </p>
+        <img src="imagens/icon2.png" class="tm">
     </div>
-
-
-    <script src="funcoes.js/jquery.js"></script>
-    <script src="lib/slick-1.8.1/slick-1.8.1/slick/slick.js"></script>
-    <script src="funcoes.js/app.js"></script>
+</footer>
 </body>
 
 </html>
