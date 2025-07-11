5cfb7d2e98750a6ae5500c5cac361be8916bfaae
Mharia <ms5132425@gmail.com>
2025-07-11 18:39:13 -0300

    ajuste no carrossel

--- index.php
+++ index.php
@@ -43,7 +43,7 @@
                     echo '<div class="carousel-item' . ($ativo ? ' active' : '') . '">';
                     echo '<div class="texto-feed">';
                     echo '<p>" ' . $linha['retorno'] . ' "</p>';
-                    echo '<h1>- ' . $linha['nome'] . '</h1>';
+                    echo '<h1 class="nome-feed">- ' . $linha['nome'] . '</h1>';
                     echo '</div></div>';
                     $ativo = false;
 
@@ -51,7 +51,7 @@
                 }
                 ?>
             </div>
-            
+
             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Previous</span>
@@ -79,18 +79,70 @@ mysqli_close($conexao);
 
 <div class="conf">
     <div class="autoplay">
-        <img src="imagens/Rectangle 11.png">
-        <img src="imagens/Rectangle 10.png">
-        <img src="imagens/Rectangle 9.png">
-        <img src="imagens/Rectangle 7.png">
+        <img src="imagens/Rectangle 26.png">
+        <img src="imagens/Rectangle 25.png">
+        <img src="imagens/Rectangle 24.png">
+         <img src="imagens/Rectangle 23.png">
+          <img src="imagens/Rectangle 29.png">
 
     </div>
 </div>
 
+<?php include "includes/conexao.php" ?>
+<div class="fundo">
+
+    <?php
+    $sql = "select * from feedback_sobrancelha";
+    $resultado = mysqli_query($conexao, $sql);
+    ?>
+        <h1 class="titulo-geral">''</h1>
+
+        <div  id="carouselExample2" class="carousel slide">
+
+            <div class="carousel-inner ">
+                <?php
+                $ativo = true;
+                while ($linha = mysqli_fetch_assoc($resultado)) {
+                    echo '<div class="carousel-item' . ($ativo ? ' active' : '') . '">';
+                    echo '<div class="texto-feed">';
+                    echo '<p>" ' . $linha['retorno'] . ' "</p>';
+                    echo '<h1 class="nome-feed"> - ' . $linha['nome'] . '</h1>';
+                    echo '</div></div>';
+                    $ativo = false;
+
+
+                }
+                ?>
+            </div>
+
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample2" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExample2" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+
+        </div>
+
+</div>
+
+<?php
+mysqli_close($conexao);
+?>
+
+
+
+
+
+
 
 
 <script src="funcoes.js/jquery.js"></script>
+
 <script src="lib/slick-1.8.1/slick-1.8.1/slick/slick.js"></script>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q"
     crossorigin="anonymous"></script>
