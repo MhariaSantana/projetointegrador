6c63c822a8bcda2b67b7492a972caa4495e1c5eb
Mharia <ms5132425@gmail.com>
2025-07-17 22:58:35 -0300

    roda pe

--- includes/rodape.php
+++ includes/rodape.php
@@ -1,6 +1,7 @@
 <footer>
     <div>
-        <img src="">
+        <div class="container">
+            <img src="">
         <div>
             <img src="imagens/insta.png"><p>beautyy_nati</p>
             <img src="imagens/zap.png"><p>(19) 989263487</p>
@@ -9,6 +10,8 @@
             <img src="imagens/loc.png"><p>seg á sex 09h00 ás 17h00</p>
             <img src="imagens/email.png"><p>e-mail : bianca@gmail.com</p>
         </div>
+        </div>
+        
     </div>
     <div class="rodape-faixa">
         <h1>Bianca Serafim LTDA</h1>
